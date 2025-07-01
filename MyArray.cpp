8aa58ce6878220df74873323453ac3e24ba5d73b
-//)Z% <f4dor2007@gmail.com>
2025-07-01 17:50:51 +0300

    Add files via upload

--- MyArray.cpp
+++ MyArray.cpp
@@ -1,8 +1,26 @@
 //-------------------------------------------------------------------------
-/* Day2_1.cpp*/
+/*MyArray.cpp*/
 #include <iostream>
 #include "MyArray.h"
+#include <fstream>
+#include <chrono>
 /* My array*/
+
+void pushTest(int trials){
+    MyArray arr1;
+    std::ofstream outfile("c:\\Users\\Student\\Desktop\\MyArray.csv");
+    for (int i=0;i<trials;i++){
+        std::chrono::_V2::system_clock::time_point sstart = std::chrono::high_resolution_clock::now();
+        arr1.pushBack(i); //Затем проверить остальные методы
+        auto elapsed = std::chrono::high_resolution_clock::now()-sstart;
+        long long milliSec=std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed).count();
+        outfile<<milliSec<<std::endl;
+    }
+}
+
+int main(){
+    pushTest(5000);
+}
 
 MyArray::MyArray(){
     size=0;
@@ -77,5 +95,16 @@ void MyArray::popBack(){
     
     --size;
 }
+
+int MyArray::getSize(){
+    return size;
+}
+int MyArray::getCatapicy(){
+    return catapicy;
+}
+int MyArray::getElem(int pos){
+    return arr[pos];
+}
+
 
 //---------------------------------------------------------------
